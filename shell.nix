6c836cfeff5e37689d29bee226b0a598c684de7c
sadiqboxed <sadiq@boxed.com>
2022-10-07 10:25:54 -0400

    Scaffolding

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
 { pkgs ? import <nixpkgs> { config.allowUnfree = true;} , ... }:
 
 let
+  node16_17_1 = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/59573f302edae493dd98d88cbe3504adb90243fc){};
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [
-      nodejs-16_x
+      node16_17_1.nodejs-16_x
       yarn
     ];
     shellHook = ''
