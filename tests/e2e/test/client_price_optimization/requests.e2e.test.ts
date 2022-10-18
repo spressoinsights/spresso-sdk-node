edf514629ef1c88cfeafc84eb3622c9d88b105f4
sadiqboxed <sadiq@boxed.com>
2022-10-18 12:35:06 -0400

    Resiliency

--- tests/e2e/test/client_price_optimization/requests.e2e.test.ts
+++ tests/e2e/test/client_price_optimization/requests.e2e.test.ts
@@ -1,4 +1,7 @@
-import { PriceOptimimizationClient as PriceOptimimizationClient_1_0 } from '@spresso-sdk/price_optimization_1.0';
+import {
+    PriceOptimimizationClient as PriceOptimimizationClient_1_0,
+    PriceOptimizationClientOptions as PriceOptimizationClientOptions_1_0,
+} from '@spresso-sdk/price_optimization_1.0';
 import { ClientSecretAuth as ClientSecretAuth_1_0 } from '@spresso-sdk/auth_1.0';
 import { InMemory } from '@spresso-sdk/cache_in_memory_1.0';
 import { expect } from 'chai';
@@ -14,14 +17,16 @@ async function testFunctionalityVersion1(client: PriceOptimimizationClient_1_0) 
 describe('Version 1.0', () => {
     describe('getPriceOptimization', () => {
         it('Can connect successfully', async () => {
-            // So here is why we might want another repo for e2e. We can have these as secrets in github actions as an alternative but it will be a pain to manage for local dev. Stil worth doing tho.
-            const client = new PriceOptimimizationClient_1_0({
+            const options = new PriceOptimizationClientOptions_1_0({
                 authenticator: new ClientSecretAuth_1_0({
                     clientId: 'foKGFuInp9llIfVIXWoa5M6fJvFZmM4E',
                     clientSecret: '7ugRF2iE7wDpJ5-IZkybHXZ2E5XRuket91HhBc-94F2MuXF6rUsL8Sl09WOdZF5I',
                 }),
                 cachingStrategy: new InMemory(),
             });
+
+            // So here is why we might want another repo for e2e. We can have these as secrets in github actions as an alternative but it will be a pain to manage for local dev. Stil worth doing tho.
+            const client = new PriceOptimimizationClient_1_0(options);
 
             await testFunctionalityVersion1(client);
         });
