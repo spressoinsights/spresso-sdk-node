66282568d2a9da056951b0c6ddc53631021c38d5
sadiqboxed <sadiq@boxed.com>
2022-10-26 10:36:11 -0400

    Option classes

--- packages/auth/src/ClientSecretAuth.ts
+++ packages/auth/src/ClientSecretAuth.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpClientOptions, HttpResponseError } from '@spresso-sdk/http_client';
 import { IAuth } from './IAuth';
+import { ClientSecretAuthOptions } from './types/models';
 import { Auth0Response } from './types/models/Auth0';
 
 type LocalAccessToken = {
@@ -8,22 +9,13 @@ type LocalAccessToken = {
 };
 
 export class ClientSecretAuth implements IAuth {
-    // can I have build flags for this?
-    private readonly url = 'https://dev-369tg5rm.us.auth0.com/oauth/token';
-
+    //private readonly url = 'https://dev-369tg5rm.us.auth0.com/oauth/token';
     private readonly httpClient: HttpClient;
-    private readonly clientId: string;
-    private readonly clientSecret: string;
 
     private localAccessToken: LocalAccessToken | undefined;
 
-    // Have a 5 minutes window to refresh the token before it expires.
-    private readonly credentialsExpireWindowMs = 300000;
-
-    constructor(options: { clientId: string; clientSecret: string }) {
+    constructor(private readonly options: ClientSecretAuthOptions) {
         this.httpClient = new HttpClient(new HttpClientOptions());
-        this.clientId = options.clientId;
-        this.clientSecret = options.clientSecret;
     }
 
     public async getAccessToken(): Promise<
@@ -31,7 +23,8 @@ export class ClientSecretAuth implements IAuth {
     > {
         if (
             this.localAccessToken == undefined ||
-            this.localAccessToken.expiresIn.getTime() - Date.now() <= this.credentialsExpireWindowMs
+            // Note: Defualt if 5 min
+            this.localAccessToken.expiresIn.getTime() - Date.now() <= this.options.credentialsExpireWindowMs
         ) {
             // add retry here
             const refreshTokenOrFailure = await this.getAndSaveAccessTokenLocally();
@@ -49,13 +42,13 @@ export class ClientSecretAuth implements IAuth {
         { success: true } | { success: false; error: HttpResponseError }
     > {
         const input = {
-            client_id: this.clientId,
-            client_secret: this.clientSecret,
+            client_id: this.options.clientId,
+            client_secret: this.options.clientSecret,
             audience: 'https://spresso-api',
             grant_type: 'client_credentials',
         };
 
-        const response = await this.httpClient.post<Auth0Response>({ url: this.url, body: input });
+        const response = await this.httpClient.post<Auth0Response>({ url: this.options.url, body: input });
 
         switch (response.kind) {
             case 'Ok': {
--- packages/auth/src/types/models/ClientSecretAuthOptions.ts
+++ packages/auth/src/types/models/ClientSecretAuthOptions.ts
@@ -1,34 +1,28 @@
-import { HttpClient } from '@spresso-sdk/http_client';
-
 type ClientSecretAuthOptionsInputType = {
-    httpClient?: HttpClient;
-
-    auth: {
-        url?: string;
-        clientId: string;
-        clientSecret: string;
-    };
-};
-
-type ClientSecretAuthOptionsOutputType = Required<ClientSecretAuthOptionsInputType> & {
-    auth: Required<ClientSecretAuthOptionsInputType['auth']>;
+    url?: string;
+    clientId: string;
+    clientSecret: string;
 };
 
 export class ClientSecretAuthOptions {
     public readonly credentialsExpireWindowMs = 300000;
 
-    public readonly auth: ClientSecretAuthOptionsOutputType['auth'];
+    public readonly url: string;
+    public readonly clientId: string;
+    public readonly clientSecret: string;
 
     constructor(options: ClientSecretAuthOptionsInputType) {
-        this.auth = this.sanitizeAuth(options);
+        this.url = this.sanitizeUrl(options);
+        this.clientId = options.clientId;
+        this.clientSecret = options.clientSecret;
     }
 
-    private sanitizeAuth(options: ClientSecretAuthOptionsInputType): ClientSecretAuthOptionsOutputType['auth'] {
-        const authDefaults = { url: 'https://api.spresso.com/oauth/token' };
-
-        const auth = { ...authDefaults, ...options.auth };
-
-        // Throws type error
-        return { ...auth, url: new URL(auth.url).href };
+    private sanitizeUrl(options: ClientSecretAuthOptionsInputType): string {
+        if (options.url != undefined) {
+            // Throws type error
+            return new URL(options.url).href;
+        } else {
+            return 'https://api.spresso.com/oauth/token';
+        }
     }
 }
