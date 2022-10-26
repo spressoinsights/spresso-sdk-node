730d443c2ebe63cebe7e0ff5010b38b364e0a90e
sadiqboxed <sadiq@boxed.com>
2022-10-26 10:50:59 -0400

    Option classes

--- tests/e2e/test/client_price_optimization/requests.e2e.test.ts
+++ tests/e2e/test/client_price_optimization/requests.e2e.test.ts
@@ -2,7 +2,10 @@ import {
     PriceOptimimizationClient as PriceOptimimizationClient_1_0,
     PriceOptimizationClientOptions as PriceOptimizationClientOptions_1_0,
 } from '@spresso-sdk/price_optimization_1.0';
-import { ClientSecretAuth as ClientSecretAuth_1_0 } from '@spresso-sdk/auth_1.0';
+import {
+    ClientSecretAuth as ClientSecretAuth_1_0,
+    ClientSecretAuthOptions as ClientSecretAuthOptions_1_0,
+} from '@spresso-sdk/auth_1.0';
 import { InMemory } from '@spresso-sdk/cache_in_memory_1.0';
 import { expect } from 'chai';
 
@@ -18,10 +21,12 @@ describe('Version 1.0', () => {
     describe('getPriceOptimization', () => {
         it('Can connect successfully', async () => {
             const options = new PriceOptimizationClientOptions_1_0({
-                authenticator: new ClientSecretAuth_1_0({
-                    clientId: 'foKGFuInp9llIfVIXWoa5M6fJvFZmM4E',
-                    clientSecret: '7ugRF2iE7wDpJ5-IZkybHXZ2E5XRuket91HhBc-94F2MuXF6rUsL8Sl09WOdZF5I',
-                }),
+                authenticator: new ClientSecretAuth_1_0(
+                    new ClientSecretAuthOptions_1_0({
+                        clientId: 'foKGFuInp9llIfVIXWoa5M6fJvFZmM4E',
+                        clientSecret: '7ugRF2iE7wDpJ5-IZkybHXZ2E5XRuket91HhBc-94F2MuXF6rUsL8Sl09WOdZF5I',
+                    })
+                ),
                 cachingStrategy: new InMemory(),
             });
 
