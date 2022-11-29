49c9f1c23207b9172f0d8d7d14f958889a46006f
sadiqboxed <sadiq@boxed.com>
2022-11-29 16:34:18 -0500

    update testing

--- tests/e2e/test/client_price_optimization/requests.e2e.test.ts
+++ tests/e2e/test/client_price_optimization/requests.e2e.test.ts
@@ -1,5 +1,6 @@
 import {
     GetPriceOptimizationInput,
+    GetPriceOptimizationsInput,
     PriceOptimimizationClient as PriceOptimimizationClient_1_0,
     PriceOptimization,
     PriceOptimizationClientOptions as PriceOptimizationClientOptions_1_0,
@@ -16,10 +17,10 @@ const clientSecretEnv = process.env['CLIENTSECRET'];
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 assertString(clientSecretEnv);
 
-async function testFunctionalityVersion1(client: PriceOptimimizationClient_1_0): Promise<void> {
+async function testGetPriceOptimization(client: PriceOptimimizationClient_1_0): Promise<void> {
     const input: GetPriceOptimizationInput = {
         deviceId: 'somedeviceid',
-        userId: 'abc',
+        userId: 'SomeUserId',
         itemId: '000001',
         defaultPrice: 11,
         userAgent: '',
@@ -33,31 +34,75 @@ async function testFunctionalityVersion1(client: PriceOptimimizationClient_1_0):
         itemId: input.itemId,
         isPriceOptimized: true,
         userId: input.userId,
-        price: input.defaultPrice,
+        price: 5,
     };
 
-    // just an example
     expect(output).to.include(res);
 }
 
+async function testGetPriceOptimizations(client: PriceOptimimizationClient_1_0): Promise<void> {
+    const input: GetPriceOptimizationsInput = {
+        userAgent: '',
+        items: [
+            {
+                deviceId: 'somedeviceid',
+                userId: 'SomeUserId',
+                itemId: '000001',
+                defaultPrice: 11,
+                overrideToDefaultPrice: false,
+            },
+        ],
+    };
+
+    const res = await client.getPriceOptimizations(input);
+
+    const output: PriceOptimization[] = [
+        {
+            deviceId: input.items[0]?.deviceId as string,
+            itemId: input.items[0]?.itemId as string,
+            isPriceOptimized: true,
+            userId: input.items[0]?.userId as string,
+            price: 5,
+        },
+    ];
+
+    expect(output).to.deep.equal(res);
+}
+
 describe('Version 1.0', () => {
-    describe('getPriceOptimization', () => {
-        it('Can connect successfully', async () => {
-            const options = new PriceOptimizationClientOptions_1_0({
-                authenticator: new ClientSecretAuth_1_0(
-                    new ClientSecretAuthOptions_1_0({
-                        url: 'https://dev-369tg5rm.us.auth0.com/oauth/token/',
-                        clientId: 'BKW7vdWHkSplXj6VshA7iEB8iiH6lNSI',
-                        clientSecret: clientSecretEnv,
-                    })
-                ),
-                cachingStrategy: new InMemory({ maxElementCount: 100, defaultTtlMs: 100000 }),
-            });
-
-            // So here is why we might want another repo for e2e. We can have these as secrets in github actions as an alternative but it will be a pain to manage for local dev. Stil worth doing tho.
-            const client = new PriceOptimimizationClient_1_0(options);
-
-            await testFunctionalityVersion1(client);
+    it('Can successfully getPriceOptimization', async () => {
+        const options = new PriceOptimizationClientOptions_1_0({
+            authenticator: new ClientSecretAuth_1_0(
+                new ClientSecretAuthOptions_1_0({
+                    url: 'https://dev-369tg5rm.us.auth0.com/oauth/token/',
+                    clientId: 'BKW7vdWHkSplXj6VshA7iEB8iiH6lNSI',
+                    clientSecret: clientSecretEnv,
+                })
+            ),
+            cachingStrategy: new InMemory({ maxElementCount: 100, defaultTtlMs: 100000 }),
         });
+
+        // So here is why we might want another repo for e2e. We can have these as secrets in github actions as an alternative but it will be a pain to manage for local dev. Stil worth doing tho.
+        const client = new PriceOptimimizationClient_1_0(options);
+
+        await testGetPriceOptimization(client);
+    });
+
+    it('Can successfully getPriceOptimizations', async () => {
+        const options = new PriceOptimizationClientOptions_1_0({
+            authenticator: new ClientSecretAuth_1_0(
+                new ClientSecretAuthOptions_1_0({
+                    url: 'https://dev-369tg5rm.us.auth0.com/oauth/token/',
+                    clientId: 'BKW7vdWHkSplXj6VshA7iEB8iiH6lNSI',
+                    clientSecret: clientSecretEnv,
+                })
+            ),
+            cachingStrategy: new InMemory({ maxElementCount: 100, defaultTtlMs: 100000 }),
+        });
+
+        // So here is why we might want another repo for e2e. We can have these as secrets in github actions as an alternative but it will be a pain to manage for local dev. Stil worth doing tho.
+        const client = new PriceOptimimizationClient_1_0(options);
+
+        await testGetPriceOptimizations(client);
     });
 });
